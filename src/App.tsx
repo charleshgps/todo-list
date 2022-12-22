b9f3458a9f9573c77d4381bcfc0571513b824ff9
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-21 23:11:35 -0300

    lista de atividades criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import * as C from "./App.styles"
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Comprar Pão", done: false },
+    { id: 2, name: "Comprar Bolo", done: false }
+  ]);
   return (
     <C.Container>
       <C.Area>
-        <C.Header>
-          Lista de Tarefas
-        </C.Header>
-        </C.Area>
+        <C.Header> Lista de Tarefas </C.Header>
+
+        {/* Area de adicionar a tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+      </C.Area>
     </C.Container>
   );
 }
