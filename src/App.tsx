e76ce50ace3b8fdf3c69b6354437a131b6cbcd82
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-27 21:06:18 -0300

    app de add task concluido

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,43 @@ const App = () => {
     { id: 1, name: "Comprar Pão", done: true },
     { id: 2, name: "Comprar Bolo", done: false }
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  // Função feita para tarefinha de casa.
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header> Lista de Tarefas </C.Header>
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
         {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+          <ListItem
+            key={index}
+            item={item}
+            onChange={handleTaskChange}
+          />
         ))}
+        {/* {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))} */}
       </C.Area>
     </C.Container>
   );
